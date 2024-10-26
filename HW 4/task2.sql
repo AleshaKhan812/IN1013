62ee14f92c9bc9b9bf629d027af5b29a895fa132
AleshaKhan812 <alesha.khan.2@city.ac.uk>
2024-10-26 13:26:22 +0100

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,25 +1,26 @@
-INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
 
--- Insert Hammy into petPet table
-INSERT INTO petPet (petname, owner, species, gender, birth, death) 
+INSERT INTO petPet (petname, owner, species, gender, birth, death)
 VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
 
--- Insert event for Hammy in petEvent table
-INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
 
-UPDATE petEvent
+UPDATE petEvent 
 SET remark = '5 kittens, 2 male, 3 female'
-WHERE petname = 'Fluffy' AND eventdate = '1995-05-15';
+WHERE petname = 'Fluffy' AND eventtype = 'litter';
 
-UPDATE petEvent
+UPDATE petEvent 
 SET petname = 'Claws'
-WHERE petname = 'Slim' AND eventdate = '1997-08-03' AND remark = 'broken rib';
+WHERE petname = 'Slim' AND eventdate = '1997-08-03';
 
-UPDATE petPet
+UPDATE petPet 
 SET death = '2020-09-01'
 WHERE petname = 'Puffball';
 
-DELETE FROM petPet
-WHERE owner = 'Harold' AND species = 'dog';
+DELETE FROM petPet 
+WHERE petname = 'Buffy' AND owner = 'Harold';
+
+DELETE FROM petEvent 
+WHERE petname = 'Buffy' AND owner = 'Harold';
