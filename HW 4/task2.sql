b426c1cd3faf7437adfc0910b460f853d7f270d9
AleshaKhan812 <alesha.khan.2@city.ac.uk>
2024-10-26 13:07:52 +0100

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,1 +1,25 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
+VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
 
+-- Insert Hammy into petPet table
+INSERT INTO petPet (petname, owner, species, gender, birth, death) 
+VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
+
+-- Insert event for Hammy in petEvent table
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
+VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
+
+UPDATE petEvent
+SET remark = '5 kittens, 2 male, 3 female'
+WHERE petname = 'Fluffy' AND eventdate = '1995-05-15';
+
+UPDATE petEvent
+SET petname = 'Claws'
+WHERE petname = 'Slim' AND eventdate = '1997-08-03' AND remark = 'broken rib';
+
+UPDATE petPet
+SET death = '2020-09-01'
+WHERE petname = 'Puffball';
+
+DELETE FROM petPet
+WHERE owner = 'Harold' AND species = 'dog';
