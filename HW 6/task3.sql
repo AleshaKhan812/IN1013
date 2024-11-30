a6019ac62f0d243fbf835c0043768582542a6068
AleshaKhan812 <alesha.khan.2@city.ac.uk>
2024-11-30 14:01:04 +0000

    Update task3.sql

--- HW 6/task3.sql
+++ HW 6/task3.sql
@@ -1,1 +1,13 @@
+-- 1
 
+SELECT  
+    restStaff.first_name, 
+    restStaff.surname
+FROM 
+    restBill
+INNER JOIN 
+    restStaff 
+ON 
+    restBill.waiter_no = restStaff.staff_no
+WHERE 
+    restBill.cust_name = 'Tanya Singh';
