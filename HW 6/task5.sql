d10fd4acd0bd12dee4a0de1399ab35bf8fb05dd4
AleshaKhan812 <alesha.khan.2@city.ac.uk>
2024-11-30 18:06:17 +0000

    Create task5.sql

---
+++ HW 6/task5.sql
@@ -0,0 +1,83 @@
+--1
+
+SELECT 
+    restStaff.first_name, 
+    restStaff.surname,
+    restBill.bill_date AS Bill_Date,
+    COUNT(restBill.bill_no) AS Bill_Count
+FROM restBill
+JOIN restStaff 
+ON restBill.waiter_no = restStaff.staff_no
+GROUP BY 
+    restStaff.first_name, 
+    restStaff.surname, 
+    restBill.bill_date
+HAVING 
+    COUNT(restBill.bill_no) >= 2;
+
+--2
+
+SELECT 
+  room_name ,
+  COUNT(*) AS Table_Count
+FROM restRest_table
+WHERE no_of_seats > 6
+GROUP BY room_name;
+
+--3
+
+SELECT 
+  restRest_table.room_name AS Room_Name, 
+  SUM(restBill.bill_total) AS Total_Bill_Amount
+FROM restBill
+JOIN restRest_table 
+ON restBill.table_no = restRest_table.table_no
+GROUP BY restRest_table.room_name;
+
+--4
+
+SELECT 
+    headwaiter.first_name AS Headwaiter_First_Name,
+    headwaiter.surname AS Headwaiter_Surname,
+    SUM(restBill.bill_total) AS Total_Bill_Amount
+FROM restBill
+JOIN restStaff AS waiter 
+ON restBill.waiter_no = waiter.staff_no
+JOIN restStaff AS headwaiter 
+ON waiter.headwaiter = headwaiter.staff_no
+GROUP BY headwaiter.first_name, headwaiter.surname
+ORDER BY Total_Bill_Amount DESC;
+
+--5
+
+SELECT 
+    subquery.cust_name AS Customer_Name,
+    subquery.Average_Spending
+FROM 
+    (SELECT 
+        restBill.cust_name,
+        AVG(restBill.bill_total) AS Average_Spending
+    FROM 
+        restBill
+    GROUP BY 
+        restBill.cust_name) AS subquery
+WHERE 
+    subquery.Average_Spending > 400;
+
+--6
+
+SELECT 
+    restStaff.first_name, 
+    restStaff.surname,
+    restBill.bill_date AS Bill_Date,
+    COUNT(restBill.bill_no) AS Bill_Count
+FROM restBill
+JOIN restStaff 
+ON restBill.waiter_no = restStaff.staff_no
+GROUP BY 
+    restStaff.first_name, 
+    restStaff.surname, 
+    restBill.bill_date
+HAVING 
+    COUNT(restBill.bill_no) >= 3;
+
