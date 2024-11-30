0ab243e6c881014d34140edfbacad7993a750bf8
AleshaKhan812 <alesha.khan.2@city.ac.uk>
2024-11-30 14:02:06 +0000

    Update task3.sql

--- HW 6/task3.sql
+++ HW 6/task3.sql
@@ -1,13 +1,9 @@
 -- 1
 
-SELECT  
-    restStaff.first_name, 
-    restStaff.surname
-FROM 
-    restBill
-INNER JOIN 
-    restStaff 
-ON 
-    restBill.waiter_no = restStaff.staff_no
-WHERE 
-    restBill.cust_name = 'Tanya Singh';
+SELECT  restStaff.first_name, restStaff.surname
+FROM restBill
+INNER JOIN restStaff 
+ON restBill.waiter_no = restStaff.staff_no
+WHERE restBill.cust_name = 'Tanya Singh';
+
+--2
